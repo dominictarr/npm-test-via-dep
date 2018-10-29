12b466914e884c2d88e758580e8b7614d1547fe5
Dominic Tarr <dominic.tarr@gmail.com>
2018-10-29 08:24:21 +0530

    get the script basically working

--- test.sh
+++ test.sh
@@ -1,7 +1,35 @@
+set -e
+dependent="$1"
 
-dependant="$1"
+commit=$(git show --format=format:%H -s)
+project=$(node -p "require('./package.json').name")
+depdir=$(pwd)
+echo "$project"@"$commit"_vs_"$dependent"
 
-commit=`git show --format=format:%H -s`
-project=`basename $(pwd)`
+testpath=/tmp/test_dependent/"$project"@"$commit"_vs_"$dependent"
+rm -rf "$testpath"
+mkdir -p "$testpath"
 
-echo "$project"@"$commit"_vs_"$dependant"
+echo $testpath
+cd $testpath
+
+##npm show "$dependent" --json > package.json
+mkdir node_modules
+npm install "$dependent" --global-style
+cd node_modules/$dependent
+npm install #to get dev deps
+cd node_modules
+# error if the dep to be tested doesn't exist
+test -e "$project" || {
+  echo "cannot test $project using $dependent" >&2
+  echo "$dependent does not depend on $project" >&2
+  exit 1
+}
+
+rm -rf "$project"
+ln -s "$depdir" "$project"
+cd ..
+npm test
+
+
+
